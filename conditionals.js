33f1f003a6fae1fccfe082ceba97950c2e0c3d91
eze1er <papaezechiel@gmail.com>
2021-10-08 17:44:43 +0000

    Commit chorus-while

--- conditionals.js
+++ conditionals.js
@@ -46,4 +46,34 @@ if (temperature < 0) {
   if (!raining) {
     console.log("Leave your umbrella at home!");
   }
+
+var time = 60;
+
+while ( time >= 0) {
+    if (time === 0) {
+        console.log("Solid rocket booster ignition and liftoff!");
+    }
+    else if ( time === 6) {
+        console.log("Main engine start");
+    }
+    else if ( time === 10) {
+        console.log("Activate main engine hydrogen burnoff system");
+    }
+    else if ( time === 16) {
+        console.log("Activate launch pad sound suppression system");
+    }
+    else if ( time === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start");
+    }
+    else if ( time === 50) {
+        console.log("Orbiter transfers from ground to internal power");
+    }
+    else {
+    console.log("T-"+time+" seconds");
+   }
+        time = time - 1;
+}
+
+
+
   //  end  here
