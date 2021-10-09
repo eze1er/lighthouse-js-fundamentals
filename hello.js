6254655dcba4a418b56968ae0c09f5109f639a15
eze1er <papaezechiel@gmail.com>
2021-10-09 08:35:28 +0000

    Commit for odd.js

--- hello.js
+++ hello.js
@@ -1,7 +1,22 @@
 const sayHello = function(name){
   console.log("Hello, " + name);
 }
+
 sayHello("Ezechiel");
 sayHello("Kakana");
 sayHello("Itimbien");
+
+const sayHelloToConsole = function(name) {
+  console.log("hello, " + name);
+}
+
+sayHelloToConsole('John');
+
+
+const returnSayHello = function(name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
 
