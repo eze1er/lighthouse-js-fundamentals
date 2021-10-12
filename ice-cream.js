b02668f116efd7ab87d88571627f2f613d04b758
eze1er <papaezechiel@gmail.com>
2021-10-12 05:08:14 +0000

    commit for icecream file

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+
+const iceCreamFlavours = ["Chocolate", "Vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer"); 
+
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
