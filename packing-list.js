e720c9649988da96e8410f214b26c912a804b421
eze1er <papaezechiel@gmail.com>
2021-10-12 05:33:35 +0000

    packing list modify

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,18 @@
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+/* const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("kichen stuff to pack: ");
 for (let i = 0; i < packingList.length; i++) { 
   console.log(packingList[i]);
+} */
+
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("kichen stuff to pack: ");
+
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
+
